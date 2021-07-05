4c812e4450378a5987baeb347949f760d0f523e8
erobbi <elirobbins0@gmail.com>
2021-07-05 11:45:36 -0400

    having issues at js:22

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-const form = document.querySelector("#form")
-console.log(form)
+let form = document.querySelector("#form")
+console.log(form);
+form.onsubmit = function(event) { // on submit
+    if (!isFormVolid(this)) {
+        event.preventDefault(); // prevent form submission if form is not valit
+    }
+}
+
+let buttonAdd = document.querySelector('#buttonAdd');
+buttonAdd.onclick = function() {console.log("thanks for clicking me");}
+buttonAdd.addEventListener("click", addDiv);
+function addDiv() {
+    console.log("thanks again, here's a new div for you")
+    block = document.createElement('div');
+    block.innerHTML = "you made me!";
+    container_block = document.getElementById( 'addToMe' );
+    container_block.appendChild( block ); 
+ }
+
+let buttonRemove = document.querySelector('#buttonRemove');
+buttonRemove.onclick = function() {console.log("thanks for clicking remove me");}
+buttonRemove.addEventListener("click", removeDiv()); // having issues here
+
+function removeDiv() {
+    console.log("thanks, let's destroy that div");
+    block =document.removeDiv();
+    container_block = document.getElementById( 'addToMe' );
+}
+
 //alert("js works")
